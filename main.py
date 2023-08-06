520f2039071aac6eaa5afd924c5484e31af57f25
a.sagiyan <a.sagiyan@dss-lab.ru>
2023-08-06 19:02:22 +0300

    mkad 109

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
-from theme1_b import NeighbouringNumbers
-
 if __name__ == '__main__':
     pass
--- theme1/theme1_a.py
+++ theme1/theme1_a.py
@@ -8,10 +8,3 @@ class Triangle:
 
     def calculate_hypotenuse(self):
         return math.sqrt(self.cat1**2 + self.cat2**2)
-
-
-side1 = int(input())
-side2 = int(input())
-
-h = Triangle(side1, side2)
-print(h.calculate_hypotenuse())
